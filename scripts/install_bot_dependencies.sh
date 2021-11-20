ca48460f15581358ce45844ecf0f50aba67b3a58
camelwater <iamaguy9999@gmail.com>
2021-11-20 00:22:29 -0600

    update shell files

--- scripts/install_bot_dependencies.sh
+++ scripts/install_bot_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 sudo pip3 install virtualenv
 cd /home/ec2-user/app
-key=$(jq .BOT_KEY ../creds/creds.json) #retrieve key
+# creds=$(jq .CREDS ../creds/credentials.json) #retrieve key
 virtualenv environment
 source environment/bin/activate
-echo KEY=$key > .env #inject key
+cat ../creds/credentials.json >> resources/credentials.json #inject key
 sudo pip3 install -r scripts/requirements.txt
