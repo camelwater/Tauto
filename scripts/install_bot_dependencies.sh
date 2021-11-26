f69614fd1b8433bece2b6dff45fb42a48790ac7e
camelwater <iamaguy9999@gmail.com>
2021-11-25 23:58:22 -0600

    try again

--- scripts/install_bot_dependencies.sh
+++ scripts/install_bot_dependencies.sh
@@ -5,5 +5,6 @@ key=$(jq .KEY ../creds/key.json) #retreive key
 virtualenv environment
 source environment/bin/activate
 echo KEY=$key > .env # inject key into .env
-cat ../creds/credentials.json >> resources/credentials.json #inject Google credentials into credentials.json
+# cat ../creds/credentials.json >> resources/credentials.json 
+cp -f ../creds/credentials.json resources/credentials.json #inject Google credentials into credentials.json
 sudo pip3 install -r scripts/requirements.txt
