2ee6baa224d475cb3979d5af2a18a88a5c0157ed
camelwater <iamaguy9999@gmail.com>
2021-11-26 00:08:02 -0600

    finally?

--- scripts/install_bot_dependencies.sh
+++ scripts/install_bot_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 sudo pip3 install virtualenv
 cd /home/ec2-user/app
+# sudo yum install jq (jq needs to be installed)
 key=$(jq .KEY ../creds/key.json) #retreive key
 virtualenv environment
 source environment/bin/activate
